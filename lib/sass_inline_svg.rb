e17630da87c4a541f2b3492b44c17a60a8dfba92
ordm <ordm67@gmail.com>
2020-10-23 13:04:17 +0300

    refactoring

--- lib/sass_inline_svg.rb
+++ lib/sass_inline_svg.rb
@@ -1,56 +1,39 @@
-require "sass_inline_svg/version"
-require "sass"
-require "cgi"
+require 'sass_inline_svg/version'
+require 'sass'
+require 'cgi'
 
 module Sass::Script::Functions
-
-  # Alias function to comply with old documentation
-  def svg_inline(path, repl = nil)
-    inline_svg(path, repl)
-  end
-  
   def inline_svg(path, repl = nil)
-    assert_type path, :String
-    path = path.value.strip()
+    assert_type(path, :String)
+    svg = read_file(path.value.strip)
+    if repl && repl.respond_to?('to_h')
+      repl = repl.to_h
+      svg = svg.to_s
+      repl.each_pair { |k, v| svg.gsub!(k.value, v.value) if svg.include? k.value }
+    end
+    encode(svg)
+  end
 
+  alias_method :inline_svg, :svg_inline
+
+  private
+
+  def encode(svg)
+    encoded = CGI::escape(svg).gsub("+", "%20")
+    encoded_url = "url('data:image/svg+xml;charset=utf-8," + encoded + "')"
+    Sass::Script::String.new(encoded_url)
+  end
+
+  def read_file(path)
     # Use Soprockets / Rails asset pipeline if in Rails context (and handle File not found):
     if defined?(Rails)
       asset = (Rails.application.assets || ::Sprockets::Railtie.build_environment(Rails.application)).find_asset(path).to_s
       raise "File not found or cannot be read (Sprockets): #{path}" if asset.nil?
-      svg = asset.to_s
-    else # otherwise read file:
-      svg = _readFile(path).strip
+
+      return asset.to_s
     end
+    raise Sass::SyntaxError, "File not found or cannot be read (native): #{path}" unless File.readable?(path)
 
-    if repl && repl.respond_to?('to_h')
-      repl = repl.to_h
-      svg = svg.to_s
-
-      repl.each_pair do |k, v|
-
-        if svg.include? k.value
-          svg.gsub!(k.value, v.value)
-        end
-      end
-    end
-
-    encoded = CGI::escape(svg).gsub("+", "%20")
-    encoded_url = "url('data:image/svg+xml;charset=utf-8," + encoded + "')"
-    Sass::Script::String.new(encoded_url)
-    
+    File.open(path, 'rb') { |f| f.read }.strip
   end
-
-
-  private
-
-  def _readFile(path)
-    if File.readable?(path)
-      File.open(path, 'rb') do |f|
-        f.read
-      end
-    else
-      raise Sass::SyntaxError, "File not found or cannot be read (native): #{path}"
-    end
-  end
-
 end
